deddcc32f45880b0b7e0f0ba53045ae5c414fd2d
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-16 19:33:29 +0300

    1

--- App.js
+++ App.js
@@ -1,37 +1,11 @@
 import React from "react";
-import {StyleSheet, Text, SafeAreaView, StatusBar, Alert, TouchableHighlight, View} from "react-native";
+import {StyleSheet, Text, SafeAreaView, StatusBar, Alert, TouchableHighlight, View, ScrollView} from "react-native";
 
 export default function App() {
-  const HandlePressWithSimpleAlert = () => Alert.alert("Simple Alert Box");
-  const HandlePressWithAdvancedAlert = () => Alert.alert(
-      'Advanced alert',
-      'My first advanced alert',
-      [
-        {
-          text: 'Cancel',
-          onPress: () => HandlePressWithSimpleAlert(),
-        },
-        {text: 'OK', onPress: () => HandlePressWithSimpleAlert()},
-      ],
-  );
-  return (
-      <View>
-        <Text onPress={HandlePressWithSimpleAlert} style = {styles.text}>
-          text with simple alert
-        </Text>
-
-        <TouchableHighlight  activeOpacity={0.6}  underlayColor="#DDDDDD"  onPress={HandlePressWithSimpleAlert}>
-            <Text style = {styles.text}>
-              text with effect
-            </Text>
-        </TouchableHighlight >
-
-        <TouchableHighlight  activeOpacity={0.6}  underlayColor="#DDDDDD"  onPress={HandlePressWithAdvancedAlert}>
-          <Text style = {styles.text}>
-            text with advanced alert
-          </Text>
-        </TouchableHighlight >
-      </View>
+    return(
+    <ScrollView>
+        <Text>123</Text>
+    </ScrollView>
   )
 }
 

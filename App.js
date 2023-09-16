937ec72f29656d1f143e25116d148200c3c723c1
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-16 12:32:27 +0300

    done

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [counter, setCounter] = React.useState(1);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setCounter((prevCount) => prevCount + 1)}
+                >
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => setCounter((prevCount) => prevCount - 1)}
+                >
+                    <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.text}>Counter: {counter}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: "100%",
+        height: "100%"
+    },
+    buttonsContainer: {
+        display: "flex",
+        flexDirection: "row",
+    },
+    button: {
+        width: 100,
+        height: 50,
+        margin: 10,
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonText: {
+        fontSize: 30,
+    },
+    text: {
+        fontSize: 32,
+    },
 });

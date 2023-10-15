8486c917882d2b7bcf1f4ebcd22ddc16e83e3223
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-10-16 00:31:37 +0300

    start

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import {Box, Button, Center, Input, NativeBaseProvider, Stack} from "native-base"
 import React from "react";
-import { NativeBaseProvider, Text, Box } from "native-base";
 
 export default function App() {
+    const [show, setShow] = React.useState(false);
+    const handleClick = () => setShow((prevState) => !prevState);
+
     return (
         <NativeBaseProvider>
-            <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-                <Text>Open up App.js to start working on your app!</Text>
+            <Box style={{}}>
+                <Input type={show ? "text" : "password"} InputRightElement={<Button ml={1} roundedLeft={0} roundedRight="md" onPress={handleClick}>
+                    {show ? "Hide" : "Show"}
+                </Button>} placeholder="Password" />
+                <Button size="lg" colorScheme="secondary" onPress={() => console.log("hello world")}>
+                    Register
+                </Button>
             </Box>
         </NativeBaseProvider>
-    );
+    )
 }

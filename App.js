c06d448cb11675aac91d598eba6a7b053edd9a60
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-10-20 01:40:41 +0300

    start

--- App.js
+++ App.js
@@ -1,150 +1,95 @@
-import {
-    Alert,
-    Box,
-    Button,
-    CloseIcon,
-    HStack,
-    Icon,
-    IconButton,
-    Input,
-    NativeBaseProvider,
-    Pressable,
-    Text,
-    VStack,
-    Collapse
-} from "native-base"
-import React from "react";
-import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import React, { Component } from 'react';
+import {StyleSheet, AppRegistry,Text, View,Animated,Easing} from 'react-native';
 
-export default function App() {
-    const [show, setShow] = React.useState(false);
-    const [isSuccess, setIsSuccess] = React.useState(false);
-    const [isError, setIsError] = React.useState(false);
-    const [name, setName] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const usersArray = [
-        { name: "John", password: 123 },
-        { name: "Alice", password: 0 },
-        { name: "Bob", password: 1 },
-        { name: "Eve", password: 2 },
-        { name: "Charlie", password: 3 },
-        { name: "Sophia", password: 4 },
-        { name: "Daniel", password: 5 },
-        { name: "Olivia", password: 6 },
-        { name: "Grace", password: 7 },
-        { name: "James", password: 8 },
-        { name: "Lily", password: 9 },
-        { name: "Michael", password: 10 }
-    ];
-
-    function handleLogIn() {
-        console.log(name,parseInt(password) )
-        console.log(usersArray.some(user => user.name === name && user.password === parseInt(password)))
-        if (usersArray.some(user => user.name === name && user.password === parseInt(password))){
-            setIsError(false);
-            setIsSuccess(true);
-        }else {
-            setIsError(true);
-            setIsSuccess(false);
-        }
+export default class App extends Component {
+    constructor() {
+        super()
+        this.animatedValue = new Animated.Value(0)
     }
-    console.log(name,password)
-    return (
 
-        <NativeBaseProvider>
+    componentDidMount() {
+        this.animate()
+    } //метод animate викликається з componentDidMount
+    animate() {
+        this.animatedValue.setValue(0)
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.linear,
+                useNativeDriver: false
+            }
+        ).start(() => this.animate())
+    }
 
-            <Box  style={{
-                flex:1,
-                alignItems: "center",
-                justifyContent: 'center'}}>
+    render() {
+        const fromTopToMid = this.animatedValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [300, 0]
+        })
+        const fromBottomToMid = this.animatedValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -300]
+        })
 
-                <Input w={{base: "75%", md: "25%"}}
-                       type={"text"}
-                       value={name}
-                       onChangeText={(text)=>setName(text)}
-                       placeholder="Name"
-                       InputLeftElement={<Icon
-                                           as={<MaterialIcons name="person" />}
-                                           size={5}
-                                           ml="2"
-                                           color="muted.400" />}
-                       />
 
-                <Input w={{base: "75%", md: "25%"}}
-                       value={password}
-                       onChangeText={(text)=>setPassword(text)}
-                       m={"30px"}
-                       type={show ? "text" : "password"}
-                       InputRightElement={<Pressable onPress={() => setShow(!show)}>
-                    <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />}
-                       size={5} mr="2" color="muted.400" />
-                </Pressable>} placeholder="Password" />
 
-                <Button onPress={handleLogIn} size={"lg"} w={"200px"}>Log In</Button>
-
-                <Collapse isOpen={isSuccess}>
-                <Alert  m={"10px"}  w="100%" status="success">
-                    <VStack space={2} flexShrink={1} w="100%">
-                        <HStack flexShrink={1} space={1} alignItems="center" justifyContent="space-between">
-                            <HStack space={2} flexShrink={1} alignItems="center">
-                                <Alert.Icon />
-                                <Text fontSize="md" fontWeight="medium" _dark={{
-                                    color: "coolGray.800"
-                                }}>
-                                    Successfully log In!
-                                </Text>
-                            </HStack>
-                            <IconButton variant="unstyled" _focus={{
-                                borderWidth: 0
-                            }} icon={<CloseIcon size="3" />} _icon={{
-                                color: "coolGray.600"
-                            }} onPress={()=>setIsSuccess(prevState => {
-                                if (prevState)
-                                    return !prevState
-                            })}/>
-                        </HStack>
-                        <Box pl="6" _dark={{
-                            _text: {
-                                color: "coolGray.600"
-                            }
-                        }}>
-                            Your are successfully log In into the app!
-                        </Box>
-                    </VStack>
-                </Alert>
-                </Collapse>
-                <Collapse isOpen={isError}>
-                    <Alert  m={"10px"}  w="100%" status="error">
-                        <VStack space={2} flexShrink={1} w="100%">
-                            <HStack flexShrink={1} space={1} alignItems="center" justifyContent="space-between">
-                                <HStack space={2} flexShrink={1} alignItems="center">
-                                    <Alert.Icon />
-                                    <Text fontSize="md" fontWeight="medium" _dark={{
-                                        color: "coolGray.800"
-                                    }}>
-                                        Wrong password or name!
-                                    </Text>
-                                </HStack>
-                                <IconButton variant="unstyled" _focus={{
-                                    borderWidth: 0
-                                }} icon={<CloseIcon size="3" />} _icon={{
-                                    color: "coolGray.600"
-                                }} onPress={()=>setIsError(prevState => {
-                                    if (prevState)
-                                        return !prevState
-                                })}/>
-                            </HStack>
-                            <Box pl="6" _dark={{
-                                _text: {
-                                    color: "coolGray.600"
-                                }
-                            }}>
-                                Please check your password or name!
-                            </Box>
-                        </VStack>
-                    </Alert>
-                </Collapse>
-            </Box>
-        </NativeBaseProvider>
-    )
+        return (
+            <View style={styles.container}>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    H
+                </Animated.Text>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    E
+                </Animated.Text>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    L
+                </Animated.Text>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    L
+                </Animated.Text>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    O
+                </Animated.Text>
+                <Animated.Text // returns Animated.Text
+                    style={{
+                        top: fromBottomToMid,
+                        marginTop: 10,
+                        color: 'green'}} >
+                    !
+                </Animated.Text>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display:"flex",
+        flexDirection:"row",
+        verticalAlign:"center",
+        alignItems:"center"
+    }
+})

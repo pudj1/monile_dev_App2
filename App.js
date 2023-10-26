8741e1bf968f772704853fb09a86306add578cd1
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-10-26 22:24:52 +0300

    done

--- App.js
+++ App.js
@@ -1,24 +1,141 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
 import {
-    StyleSheet,
-    Text,
     View,
-    Animated,
-    Easing,
+    Text,
+    Button,
     Dimensions,
-    ScrollView,
+    StyleSheet,
     SafeAreaView,
-    ImageBackground,
+    ScrollView,
+    Animated,
+    ImageBackground
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
- return (
-     <View>
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{margin:10}}>
+            <Button
+                title="Go to Contact Book"
+                onPress={() => navigation.navigate('Contact Book')}
+            />
+            </View>
+            <View style={{margin:10}}>
+            <Button
+                title="Go to Gallery"
+                onPress={() => navigation.navigate('Gallery')}
+            />
+            </View>
+            <View style={{margin:10}}>
+            <Button
+                title="Go to Something"
+                onPress={() => navigation.navigate('smth')}
+            />
+            </View>
+        </View>
+    );
+}
 
-     </View>
- );
-};
 
+function ContactBook({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Contact Book Screen</Text>
+            <Button
+                title="Go back"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    );
+}
+function Gallery({ navigation }) {
+    const windowWidth = Dimensions.get('window').width;
+    const images = [
+        "https://png.pngtree.com/png-clipart/20230514/original/pngtree-smile-dog-on-white-background-png-image_9160783.png",
+        "https://png.pngtree.com/png-clipart/20231003/original/pngtree-cute-husky-dog-animal-png-image_13076108.png",
+        "https://png.pngtree.com/png-clipart/20230417/original/pngtree-cute-white-dog-png-image_9058856.png",
+        "https://png.pngtree.com/png-clipart/20230411/original/pngtree-dog-corgi-animal-cute-white-background-transparent-png-image_9044376.png",
+        "https://png.pngtree.com/png-clipart/20231003/original/pngtree-puppy-labrador-retriever-cute-dog-animal-png-image_13075558.png"
+    ]
+    const scrollX = new Animated.Value(0);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <SafeAreaView style={styles.container3}>
+                <View style={styles.scrollContainer}>
+                    <ScrollView
+                        horizontal={true}
+                        style={styles.scrollViewStyle}
+                        pagingEnabled
+                        showsHorizontalScrollIndicator={false}
+                        onScroll={Animated.event(
+                            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                            { useNativeDriver: false }
+                        )}
+                        scrollEventThrottle={1}
+                    >
+                        {images.map((image, imageIndex) => {
+                            return (
+                                <View style={{ width: windowWidth, height: 250 }} key={imageIndex}>
+                                    <ImageBackground source={{ uri: image }} style={styles.card}>
+
+                                    </ImageBackground>
+                                </View>
+                            );
+                        })}
+                    </ScrollView>
+                    <View style={styles.indicatorContainer}>
+                        {images.map((image, imageIndex) => {
+                            const width = scrollX.interpolate({
+                                inputRange: [
+                                    windowWidth * (imageIndex - 1),
+                                    windowWidth * imageIndex,
+                                    windowWidth * (imageIndex + 1),
+                                ],
+                                outputRange: [8, 16, 8],
+                                extrapolate: 'clamp',
+                            });
+                            return <Animated.View key={imageIndex} style={[styles.normalDot, { width }]} />;
+                        })}
+                    </View>
+                </View>
+            </SafeAreaView>
+            <Button
+                title="Go back"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    );
+}
+function Something({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Something Screen</Text>
+            <Button
+                title="Go back"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    );
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Contact Book" component={ContactBook} />
+                <Stack.Screen name="Gallery" component={Gallery} />
+                <Stack.Screen name="smth" component={Something} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -31,7 +148,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         height: Dimensions.get('window').height,
-        backgroundColor: 'white', 
+        backgroundColor: 'white',
     },
     container3: {
         flex: 1,
